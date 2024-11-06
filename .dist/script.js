750b28b2b9c82d67fd210ff77d0b49f40dd8d621
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-06 17:02:55 -0300

    .\.dist\

---
+++ .dist/script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Obrigado por entrar em contato! Nós responderemos em breve.");
+});
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Obrigado por entrar em contato! Nós responderemos em breve.");
+});
